7b94b456bbff16dc05553ebb1ca8e438065e4faf
omid-sharifi <omid.sharifi@toros.edu.tr>
2023-08-09 11:48:56 +0300

    Create Edit Listing page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import EditListing from "./pages/EditListing";
 
 
 function App() {
@@ -27,9 +28,15 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />}  />
-          <Route path="/create-listing" element={<PrivateRoute/>}>
+
+          <Route path="create-listing" element={<PrivateRoute/>}>
              <Route path="/create-listing" element={<CreateListing/>}  />
             </Route> 
+
+            <Route path="edit-listing" element={<PrivateRoute/>}>
+             <Route path="/edit-listing/:listingId" element={<EditListing/>}  />
+            </Route> 
+
         </Routes>
       </Router>
       <ToastContainer
